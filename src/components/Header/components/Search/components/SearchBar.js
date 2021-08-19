a27ae52af9c10f10fddfaf7131e1213dbc41f344
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-08-19 16:53:41 +0100

    Refactor files

--- src/components/Header/components/Search/components/SearchBar.js
+++
@@ -1,5 +0,0 @@
-function SearchBar() {
-  return <input className="search-bar" placeholder="Search mail" />;
-}
-
-export default SearchBar;
---
+++ src/components/Header/components/Search/components/SearchBar/index.js
@@ -0,0 +1,5 @@
+function SearchBar() {
+  return <input className="search-bar" placeholder="Search mail" />;
+}
+
+export default SearchBar;
